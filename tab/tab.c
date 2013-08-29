de12f72342183be26246c614574a139c6170f5a7
Nicolas <nicolas@ubuntu.(none)>
2013-08-29 16:44:19 +0200

    perf modifié tab et memoryfork

--- tab/tab.c
+++ tab/tab.c
@@ -10,32 +10,36 @@
 
 #define MAX_SIZE 4000
 #define MULTIPLICATEUR 100
+
+timer * t ;
+recorder * lig_rec;
+recorder * col_rec;
 	
 
 /**
 	On parcourt le double tableau par ligne, en écrivant les performances dans le record
 */
-void ligne(int i, recorder* lig_rec, timer *t, int ** tab) {
+long int ligne(int i, timer *t, int ** tab) {
 	int m,n,res=0;
 	start_timer(t);
 	for(m=0; m<i; m++) 
 		for(n=0; n<i; n++) 	
 			res+= tab[n][m];
 		
-	write_record_n(lig_rec, i/MULTIPLICATEUR, stop_timer(t), MULTIPLICATEUR);
+	return stop_timer(t);
 }
 
 /**
 	On parcourt le double tableau par colonne, en écrivant les performances dans le record
 */
-void colonne(int i, recorder* col_rec, timer *t, int ** tab) {
+long int colonne(int i, timer *t, int ** tab) {
 	int m,n,res=0;
 	start_timer(t);
 	for(m=0; m<i; m++) 
 		for(n=0; n<i; n++) 	
 			res+= tab[m][n];
 		
-	write_record_n(col_rec, i/MULTIPLICATEUR, stop_timer(t), MULTIPLICATEUR);
+	 return stop_timer(t);
 }
 
 /**
@@ -52,12 +56,15 @@ int main (int argc, char *argv[])  {
   	int perfligne = argc>1 && strncmp(argv[1], "--ligne", 8);
 	int perfcolonne = argc>1 &&strncmp(argv[1], "--colonne", 10);
 
-	// Déclare un timer, ainsi que deux recorder qui vont contenir les résultats de l'exécution du programme
 	timer * t = timer_alloc();
-	recorder * lig_rec = recorder_alloc("tab-lig.csv");
- 	recorder * col_rec = recorder_alloc("tab-col.csv");
-		
+	if(!perfligne && !perfcolonne) {
+		// Déclare un timer, ainsi que deux recorder qui vont contenir les résultats de l'exécution du programme
+		lig_rec = recorder_alloc("tab-lig.csv");
+	 	col_rec = recorder_alloc("tab-col.csv");
+	}
+
 	int i,j;
+	long int rescol, reslig;
 	for(i=MULTIPLICATEUR; i<MAX_SIZE ; i+=MULTIPLICATEUR) {
 	
 		if(perfligne || perfcolonne)
@@ -73,9 +80,14 @@ int main (int argc, char *argv[])  {
 			tab[j] = malloc(i*sizeof(int));
 
 		if(!perfcolonne)
-			ligne(i, lig_rec, t, tab);
+			reslig = ligne(i, t, tab);
 		if(!perfligne)
-			colonne(i, col_rec, t, tab);
+			rescol = colonne(i, t, tab);
+
+		if(!perfligne && !perfcolonne) {
+			write_record_n(col_rec, i/MULTIPLICATEUR, rescol, MULTIPLICATEUR);
+			write_record_n(lig_rec, i/MULTIPLICATEUR, reslig, MULTIPLICATEUR);
+		}
 
 		//On libère la mémoire
 		for(j=0; j<i; j++) {
@@ -85,10 +97,11 @@ int main (int argc, char *argv[])  {
 		free(tab);
 	}
 	
-	 
-	recorder_free(lig_rec);
-  	recorder_free(col_rec);
-	timer_free(t);
+	if(!perfligne && !perfcolonne) {
+		recorder_free(lig_rec);
+	  	recorder_free(col_rec);
+		timer_free(t);
+	}
 
 	return EXIT_SUCCESS;
 }
