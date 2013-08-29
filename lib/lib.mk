e4cc89bf0750dc6e33a8569e980d515f75630006
Benoît Legat <benoit.legat@gmail.com>
2013-08-29 10:49:30 +0200

    Add docs in lib.mk and make xdg-open be open on DARWIN

--- lib/lib.mk
+++ lib/lib.mk
@@ -1,3 +1,17 @@
+#//! \file lib.mk
+#//! \brief Rassemble les règles communes à tous les `Makefile`s
+#//!
+#//! À inclure depuis le `Makefile` d'un benchmark en spécifiant les constantes
+#//! suivantes
+#//! * `CFLAGS`, les `options` supplémentaires qu'il faut donner
+#//!             à `gcc`;
+#//! * `OBJ`, les objects à générer en plus de `benchmark.o`;
+#//! * `DEPS`, les `.h` nécessaires en plus de `benchmark.h`;
+#//! * `GRAPHS`, les `.csv` générés;
+#//! * `prog`, le nom du programme;
+#//! * `tmp`, les fichiers temporaires générés par le programme
+#//!          qu'il faut supprimer avec `make clean`.
+
 # TODO mesure function call
 # TODO memcpy taille de la zone mémoire, alignement 32 Bits (remove packed)
 # TODO matrice ligne par ligne colonne par colonne
@@ -10,13 +24,15 @@ CC      = gcc
 OS := $(shell uname)
 CFLAGS += -I. -I../lib/ -g -Wall
 ifeq ($(OS), Darwin)
+  OPEN = open
 else
-CFLAGS += -lrt
+  OPEN = xdg-open
+  CFLAGS += -lrt
 endif
 
 # Lorsqu'on fait `make`, c'est la première règle qui est exécutée
 # donc dans ce cas-ci, c'est sa dependance.
-default: show_html
+default: show-html
 
 # $@ c'est la règle, ici le nom de l'objet comme `benchmark.o`
 # $< c'est $< c'est le premier élément de la liste des dépendances, ici
@@ -33,7 +49,7 @@ valgrind: $(PROG)
 $(GRAPHS): $(PROG)
 	./$(PROG)
 
-show_graph: $(GRAPHS)
+show-graph: $(GRAPHS)
 	gnuplot -p $(PROG).gpi
 
 $(PROG).png: $(GRAPHS) $(PROG).gpi
@@ -43,8 +59,8 @@ $(PROG).png: $(GRAPHS) $(PROG).gpi
 index.html: $(PROG).png ../lib/gen_html.sh README.html
 	../lib/gen_html.sh $(PROG) > index.html
 
-show_html: index.html
-	xdg-open index.html &
+show-html: index.html
+	$(OPEN) index.html &
 
 .PHONY: run show clean default mrproper valgrind
 
