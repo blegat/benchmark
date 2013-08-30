0fb719da57c7e016af3817dd984d82fff2a4b51d
HappyRave <maxime.demol@student.uclouvain.be>
2013-08-30 09:58:29 +0200

    Pull upstream

--- shm/shm.c
+++ shm/shm.c
@@ -2,53 +2,111 @@
 #include <stdlib.h>
 #include <sys/shm.h>
 #include <fcntl.h>
+#include <pthread.h>
+#include <err.h>
+#include <unistd.h>
 
 #include "benchmark.h"
 
-#define MAX_SIZE 0x100000 // 1 MB
+#define ARRAY_LEN 50000
+#define KEY 1252
+#define DO 20
+
+void* work (void* param) {
+	int* array = (int*) param;
+	int size = array[0];
+	int i;
+	int tot;
+	for (i = 1; i<size; i++) {
+		tot += array[i];
+	}
+	array[size-1] = tot;
+	//printf("%d\n",tot);
+	return NULL;
+}
 
 int main (int argc, char *argv[])  {
-  timer *t = timer_alloc();
-  recorder *shmget_rec = recorder_alloc("shmget.csv");
-  recorder *shmat_rec = recorder_alloc("shmat.csv");
-  recorder *shmdt_rec = recorder_alloc("shmdt.csv");
+	timer *t = timer_alloc();
+	recorder *heap_rec = recorder_alloc("heap.csv");
+	recorder *shm_rec = recorder_alloc("shm.csv");
+	pthread_t thread;
+	
+	int i;
+	for (i=100; i<=ARRAY_LEN; i+=100) {
+		start_timer(t);
+		int* array = (int*)malloc(sizeof(int)*i);
+		
+		int k;
+		for (k=0; k<DO; k++) {
+		
+			int j;
+			for (j=0; j<i; j++) {
+				array[j] = j+1;
+			}
+			array[0] = i;
+			pthread_create(&thread,NULL,work,(void*)array);
+			pthread_join(thread,NULL);
+		}
+		write_record_n(heap_rec,i,stop_timer(t),ARRAY_LEN);
+		free(array);
+	}
+	
+	for (i=100; i<=ARRAY_LEN; i+=100) {
+		
+		int shm_id = shmget(KEY, sizeof(int)*i, IPC_CREAT | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP );
+		if (shm_id < 0) err(-1, "erreur lors de shmget");
+		int* array = shmat(shm_id,NULL,0);
+		//if (array = -1) err(-1, "erreur lors de shmat");
+		
+		start_timer(t);
+		
+		int k;
+		for (k=0; k<DO; k++) {
+		
+			int j;
+			for (j=0; j<i; j++) {
+				array[j] = j+1;
+			}
+			array[0] = i;
+		
+			pid_t pid = fork();
+		
+			if (pid < 0) {
+				err(pid,"erreur de fork");
+			} else if (pid == 0) {
+				/*
+				int shm_id = shmget(KEY, sizeof(int)*i, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP );
+				if (shm_id < 0) err(-1, "erreur lors de shmget dans le fils");
+				int* array = shmat(shm_id,NULL,0);
+				//if (array = (void*)-1) err(-1, "erreur lors de shmat dans le fils");
+				 */
+				int size = array[0];
+				int i;
+				int tot;
+				for (i = 1; i<size; i++) {
+					tot += array[i];
+				}
+				array[size-1] = tot;
+				//printf("%d\n",tot);
+				int dt = shmdt(array);
+				if (dt < 0) err(dt,"erreur lors de shmdt dans le fils");
+				exit(0);
+			} else {
+				waitpid(pid,NULL,0);
+			}
+		}
+		write_record_n(shm_rec,i,stop_timer(t),ARRAY_LEN);
+		int ctl = shmctl(shm_id, IPC_RMID, NULL);
+		if (ctl < 0) err(ctl,"erreur lors de shmctl");
+		int dt = shmdt(array);
+		if (dt < 0) err(dt,"erreur lors de shmdt");
+		shmctl(shm_id, IPC_RMID, NULL);
+				
+	}
 
-  size_t size;
-  int err;
-  key_t key=1252;
-  for (size = getpagesize(); size <= MAX_SIZE; size *= 2) {
-    key++;
-    // BEGIN
-    start_timer(t);
-    int shm_id = shmget(key, size, IPC_CREAT | S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-    write_record(shmget_rec, size, stop_timer(t));
-    if (shm_id == -1) {
-      perror("shmget");
-      exit(EXIT_FAILURE);
-    }
+	recorder_free(heap_rec);
+	recorder_free(shm_rec);
+	timer_free(t);
 
-    start_timer(t);
-    void * addr = shmat(shm_id, NULL, 0);
-    write_record(shmat_rec, size, stop_timer(t));
-    if (addr == (void *) -1) {
-      perror("shmat");
-      exit(EXIT_FAILURE);
-    }
-
-    start_timer(t);
-    err = shmdt(addr);
-    write_record(shmdt_rec, size, stop_timer(t));
-    if (err == -1) {
-      perror("shmdt");
-      exit(EXIT_FAILURE);
-    }
-    // END
-  }
-
-  recorder_free(shmget_rec);
-  recorder_free(shmat_rec);
-  recorder_free(shmdt_rec);
-  timer_free(t);
-
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
